cad24e0e2b397906d7d11907ce9de6e02601d6b6
Ashwin1729 <mathematicswroteus@gmail.com>
2023-08-26 13:54:52 +0530

    product model updated

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -23,7 +23,7 @@ const Product = sequelize.define("product", {
     allowNull: false,
   },
   quantity: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.DOUBLE,
     allowNull: false,
   },
 });
